803fe0405ba50afaf58fa9dc8b5b179fa71f6af3
birchmd <birchmd@casperlabs.io>
2019-04-12 20:28:41 -0400

    LMD GHOST implementation

--- src/main/scala/io/casperlabs/sim/blockchain_models/casperlabs_classic/blocks.scala
+++ src/main/scala/io/casperlabs/sim/blockchain_models/casperlabs_classic/blocks.scala
@@ -2,9 +2,11 @@ package io.casperlabs.sim.blockchain_models.casperlabs_classic
 
 import io.casperlabs.sim.blockchain_components.execution_engine.Transaction
 import io.casperlabs.sim.blockchain_components.hashing.{FakeHashGenerator, HashValue}
+import io.casperlabs.sim.simulation_framework.AgentId
 
 sealed abstract class Block {
   def id: HashValue
+  def creator: AgentId // TODO: create specific Validator ID type?
   def dagLevel: Int
   def parents: IndexedSeq[Block]
   def justifications: IndexedSeq[Block]
@@ -13,6 +15,7 @@ sealed abstract class Block {
 
 case object Genesis extends Block {
   override val id: HashValue = FakeHashGenerator.nextHash()
+  override val creator: AgentId = -1 // no one created genesis
   override def dagLevel: Int = 0
   override def parents: IndexedSeq[Block] = IndexedSeq.empty
   override def justifications: IndexedSeq[Block] = IndexedSeq.empty
@@ -21,7 +24,7 @@ case object Genesis extends Block {
 
 case class NormalBlock(
                   id: HashValue,
-                  creator: Node,
+                  creator: AgentId,
                   dagLevel: Int,
                   parents: IndexedSeq[Block],
                   justifications: IndexedSeq[Block],
