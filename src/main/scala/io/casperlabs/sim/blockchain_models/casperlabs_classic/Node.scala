dfef4c8e68bad955a6b1c26ef98e96b84b33cba2
birchmd <birchmd@casperlabs.io>
2019-04-12 20:45:33 -0400

    CON-191: Node proposes a block

--- src/main/scala/io/casperlabs/sim/blockchain_models/casperlabs_classic/Node.scala
+++ src/main/scala/io/casperlabs/sim/blockchain_models/casperlabs_classic/Node.scala
@@ -2,12 +2,15 @@ package io.casperlabs.sim.blockchain_models.casperlabs_classic
 
 import io.casperlabs.sim.blockchain_components.{Discovery, DoublyLinkedDag, Gossip}
 import io.casperlabs.sim.blockchain_components.execution_engine.{Account, Transaction}
+import io.casperlabs.sim.blockchain_components.hashing.FakeHashGenerator
+import io.casperlabs.sim.simulation_framework.Agent.MsgHandlingResult
 import io.casperlabs.sim.simulation_framework.{Agent, AgentId, SimEventsQueueItem}
 
 import scala.collection.mutable
 
 class Node(
-  override val id: AgentId, 
+  override val id: AgentId,
+  stakes: Map[AgentId, Int], // TODO: have this information in block instead
   account: Account,
   d: Discovery[AgentId, AgentId],
   g: Gossip[AgentId, AgentId, Node.Comm],
@@ -20,29 +23,32 @@ class Node(
   private val jDag: DoublyLinkedDag[Block] = DoublyLinkedDag.jBlockDag(genesis)
 
   override def handleMsg(msg: SimEventsQueueItem.AgentToAgentMsg[Node.Comm, Node.Operation]): Agent.MsgHandlingResult[Node.Comm] = msg.payload match {
-    case n @ Node.Comm.NewBlock(b) => 
-      addBlock(b) match {
-        case Node.AddBlockResult.AlreadyAdded => 
-          // We got this block already, nothing to do
-          Agent.MsgHandlingResult(Nil, 0L)
-
-        case Node.AddBlockResult.MissingJustifications(_) =>
-          // We can't add this block yet, nothing to send out at this time
-          Agent.MsgHandlingResult(Nil, 0L)
-
-        case Node.AddBlockResult.Invalid => 
-          // Something is wrong with the block.
-          // No new messages need to be sent. 
-          // TODO: slashing
-          Agent.MsgHandlingResult(Nil, 0L)
-
-        case Node.AddBlockResult.Valid =>
-          // Block is valid, gossip to others.
-          g.gossip(n)
-          // TODO: Should this somehow expend time?
-          Agent.MsgHandlingResult(Nil, 0L)
-      }
+    case Node.Comm.NewBlock(b) =>
+      handleBlock(b)
   }
+
+  def handleBlock(b: Block): MsgHandlingResult[Node.Comm] =
+    addBlock(b) match {
+      case Node.AddBlockResult.AlreadyAdded =>
+        // We got this block already, nothing to do
+        Agent.MsgHandlingResult(Nil, 0L)
+
+      case Node.AddBlockResult.MissingJustifications(_) =>
+        // We can't add this block yet, nothing to send out at this time
+        Agent.MsgHandlingResult(Nil, 0L)
+
+      case Node.AddBlockResult.Invalid =>
+        // Something is wrong with the block.
+        // No new messages need to be sent.
+        // TODO: slashing
+        Agent.MsgHandlingResult(Nil, 0L)
+
+      case Node.AddBlockResult.Valid =>
+        // Block is valid, gossip to others.
+        g.gossip(Node.Comm.NewBlock(b))
+        // TODO: Should this somehow expend time?
+        Agent.MsgHandlingResult(Nil, 0L)
+    }
 
   override def handleExternalEvent(event: SimEventsQueueItem.ExternalEvent[Node.Comm, Node.Operation]): Agent.MsgHandlingResult[Node.Comm] = event.payload match {
     case d: Node.Operation.Deploy => 
@@ -50,7 +56,27 @@ class Node(
       Agent.MsgHandlingResult(Nil, 0L) // No new messages need to be sent
 
     case Node.Operation.Propose =>
-      ??? // Propose a new block
+      // Propose a new block
+      // TODO: check for equivocations?
+      val latestMessages = jDag.tips.groupBy(_.creator).mapValues(_.head)
+      val parents = BlockdagUtils.lmdGhost(
+        latestMessages,
+        stakes,
+        pDag,
+        genesis
+      )
+      // TODO: use execution engine to process deploys
+      val txns = deployBuffer.toIndexedSeq.map(_.t)
+      deployBuffer.clear()
+      val block = NormalBlock(
+        FakeHashGenerator.nextHash(),
+        id,
+        parents.map(_.dagLevel).max + 1,
+        parents,
+        latestMessages.values.toIndexedSeq,
+        txns
+      )
+      handleBlock(block)
   }
 
   override def startup(): Unit = ???
